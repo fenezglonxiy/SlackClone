9a141777eebefa079d6064368e453a45c03b7d68
fenezglonxiy <thdathcmus@gmail.com>
2024-08-30 20:13:31 +0700

    #1 - Init Form component from shadcn

---
+++ src/components/form/FormDescription.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import useFormField from "./useFormField";
+import { cn } from "@/lib/utils";
+
+const FormDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => {
+  const { formDescriptionId } = useFormField();
+
+  return (
+    <p
+      ref={ref}
+      id={formDescriptionId}
+      className={cn("text-sm text-muted-foreground", className)}
+      {...props}
+    />
+  );
+});
+FormDescription.displayName = "FormDescription";
+
+export default FormDescription;
---
+++ src/components/form/index.ts
@@ -0,0 +1,8 @@
+export { default as Form } from "./Form";
+export { default as FormControl } from "./FormControl";
+export { default as FormDescription } from "./FormDescription";
+export { default as FormField } from "./FormField";
+export { default as FormItem } from "./FormItem";
+export { default as FormLabel } from "./FormLabel";
+export { default as FormMessage } from "./FormMessage";
+export { default as useFormField } from "./useFormField";
