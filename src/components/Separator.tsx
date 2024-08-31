a2df9df81d90fbc81addbaec501849ea51980470
fenezglonxiy <thdathcmus@gmail.com>
2024-08-31 10:49:19 +0700

    #1 - Create Separator component

---
+++ src/components/Separator.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import React from "react";
+import { Separator as PrimitiveSeparator } from "@radix-ui/react-separator";
+
+import { cn } from "@/lib/utils";
+
+const Separator = React.forwardRef<
+  React.ElementRef<typeof PrimitiveSeparator>,
+  Omit<React.ComponentPropsWithoutRef<typeof PrimitiveSeparator>, "asChild">
+>(
+  (
+    {
+      className,
+      orientation = "horizontal",
+      decorative = true,
+      children,
+      ...props
+    },
+    ref
+  ) => {
+    const ContentWithChildren = (
+      <div
+        className={cn(
+          "flex items-center [&_hr]:flex-grow [&_hr]:border-[#ddd]",
+          orientation === "horizontal" && "[&_div]:mx-5",
+          orientation === "vertical" &&
+            "flex-col [&_hr]:border-t-0 [&_hr]:border-l-[1px] [&_div]:my-5"
+        )}
+      >
+        <hr />
+        <div>{children}</div>
+        <hr />
+      </div>
+    );
+
+    const ContentWithoutChildren = <hr className="border-[#ddd]" />;
+
+    const Content =
+      children === undefined ? ContentWithoutChildren : ContentWithChildren;
+
+    return (
+      <PrimitiveSeparator
+        ref={ref}
+        decorative={decorative}
+        orientation={orientation}
+        asChild
+        {...props}
+      >
+        {Content}
+      </PrimitiveSeparator>
+    );
+  }
+);
+Separator.displayName = PrimitiveSeparator.displayName;
+
+export default Separator;
