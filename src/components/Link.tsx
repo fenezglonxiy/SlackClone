9efdad8b7df39ab92ce6b170a943175fdb383c71
fenezglonxiy <thdathcmus@gmail.com>
2024-08-31 10:49:50 +0700

    #1 - Create Link component

---
+++ src/components/Link.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import { default as NextLink } from "next/link";
+import Button from "./Button";
+
+export type LinkProps = React.ComponentPropsWithoutRef<typeof NextLink>;
+
+const Link = React.forwardRef<React.ElementRef<typeof NextLink>, LinkProps>(
+  ({ className, ...props }, ref) => (
+    <Button variant="link" className={className} asChild>
+      <NextLink ref={ref} {...props} />
+    </Button>
+  ),
+);
+
+Link.displayName = "Link";
+export default Link;
