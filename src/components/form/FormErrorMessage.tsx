12342be5a23b4645afbec4f710f34567f9a19924
fenezglonxiy <thdathcmus@gmail.com>
2024-09-23 20:44:40 +0700

    #1 - Update FormErrorMessage component

--- src/components/form/FormErrorMessage.tsx
+++ src/components/form/FormErrorMessage.tsx
@@ -16,56 +16,85 @@ export type FormErrorMessageProps = Omit<
    * Control the prefix icon.
    */
   icon?: React.ReactNode;
+
+  /**
+   * Control the Loader component to render when loading the error.
+   */
+  loader?: React.ReactNode;
+
+  /**
+   * Control whether Loader component should be enabled.
+   *
+   * If no Loader component was provided, default Loader component would
+   * be used.
+   */
+  loaderEnabled?: boolean;
 };
 
 const FormErrorMessage = React.forwardRef<
   HTMLParagraphElement,
   FormErrorMessageProps
->(({ className, placeholderEnabled, icon, ...props }, ref) => {
-  const { formState } = useFormContext();
-  const { errors, isSubmitting } = formState;
-  const serverError = errors.root?.serverError;
+>(
+  (
+    {
+      className,
+      placeholderEnabled,
+      icon,
+      loader: loaderFromProps,
+      loaderEnabled,
+      ...props
+    },
+    ref,
+  ) => {
+    const { formState } = useFormContext();
+    const { errors, isSubmitting } = formState;
+    const serverError = errors.root?.serverError;
 
-  if (!serverError && !placeholderEnabled) {
-    return null;
-  }
+    if (!serverError && !placeholderEnabled) {
+      return null;
+    }
 
-  // const loader = loaderFromProps ?? (
-  //   <div className="space-x-3">
-  //     <i className="fa-light fa-spinner fa-xl animate-spin" />
+    const isLoaderEnabled = loaderEnabled || (loaderFromProps as boolean);
 
-  //     <span>Checking your code...</span>
-  //   </div>
-  // );
+    const loader = loaderFromProps ?? (
+      <div className="space-x-3">
+        <i className="fa-light fa-spinner fa-xl animate-spin" />
 
-  return (
-    <Typography
-      as="div"
-      variant="xs"
-      className={cn(
-        "rounded-lg px-4 py-3",
-        serverError && "bg-[#ff81aa21]",
-        className,
-      )}
-    >
-      {!serverError && placeholderEnabled && !isSubmitting && (
-        <p className="invisible opacity-0">&nbsp;</p>
-      )}
+        <span>Checking your code...</span>
+      </div>
+    );
 
-      {serverError && (
-        <p
-          ref={ref}
-          className={cn(icon && "space-x-3 [&_i]:text-[#c0135b]")}
-          {...props}
-        >
-          {icon}
+    return (
+      <Typography
+        as="div"
+        variant="xs"
+        className={cn(
+          "rounded-lg px-4 py-3",
+          serverError && "bg-[#ff81aa21]",
+          className,
+        )}
+      >
+        {!serverError && placeholderEnabled && !isSubmitting && (
+          <p className="invisible opacity-0">&nbsp;</p>
+        )}
 
-          <span>{serverError.message}</span>
-        </p>
-      )}
-    </Typography>
-  );
-});
+        {!serverError && isLoaderEnabled && isSubmitting && loader}
+
+        {serverError && (
+          <p
+            ref={ref}
+            className={cn(icon && "space-x-3 [&_i]:text-[#c0135b]")}
+            {...props}
+          >
+            {icon}
+
+            <span>{serverError.message}</span>
+          </p>
+        )}
+      </Typography>
+    );
+  },
+);
 FormErrorMessage.displayName = "FormErrorMessage";
 
 export default FormErrorMessage;
